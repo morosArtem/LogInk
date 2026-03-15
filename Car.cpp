f1958bb53044511c7ae84b9777a5afc958718eec
noble_kiwi <iwkr1208@gmail.com>
2026-03-15 16:05:13 +0300

    Car.cpp

--- Car.cpp
+++ Car.cpp
@@ -1,69 +1,29 @@
 #include "Car.h"
+#include "TextureHolder.h"
 
-//Реализация класса автомобиля
-Car::Car() {
-	m_Speed = 400;
-
-	m_Texture.loadFromFile("Image/Car.png");
-	m_Sprite.setTexture(m_Texture);
-
-	m_Position.x = 10;
-	m_Position.y = 25;
+Car::Car(InputHandler& input) : m_Input(input) {
+    m_Speed = 400.0f;
+    // Load the car texture using the TextureHolder singleton.
+    m_Sprite.setTexture(TextureHolder::getTexture("Image/Car.png"));
+    m_Position = {10.0f, 25.0f};
+    m_Sprite.setPosition(m_Position);
 }
 
-Sprite Car::getSprite() {
-	return m_Sprite;
-};
-
-void Car::moveUp() {
-	m_UpPressed = true;
-};
-void Car::moveDown() {
-	m_DownPressed = true;
-};
-
-void Car::stopUp() {
-	m_UpPressed = false;
-};
-void Car::stopDown() {
-	m_DownPressed = false;
-};
-void Car::moveRight() {
-	m_RightPressed = true;
-};
-void Car::moveLeft() {
-	m_LeftPressed = true;
-};
-
-void Car::stopRight() {
-	m_RightPressed = false;
-};
-void Car::stopLeft() {
-	m_LeftPressed = false;
-};
-
-void Car::update(float elapsedTime)
-{
-	if (m_UpPressed)
-	{
-		m_Position.y -= m_Speed * elapsedTime;
-	}
-
-	if (m_DownPressed)
-	{
-		m_Position.y += m_Speed * elapsedTime;
-	}
-
-	if (m_LeftPressed)
-	{
-		m_Position.x -= (m_Speed * elapsedTime)/2;
-	}
-
-	if (m_RightPressed)
-	{
-		m_Position.x += (m_Speed * elapsedTime)*1.25;
-	}
-	
-	m_Sprite.setPosition(m_Position);
-
+void Car::update(float elapsedTime) {
+    // Move according to pressed keys.
+    // Note: horizontal speeds are intentionally different (left slower, right faster)
+    if (m_Input.isUpPressed()) {
+        m_Position.y -= m_Speed * elapsedTime;
+    }
+    if (m_Input.isDownPressed()) {
+        m_Position.y += m_Speed * elapsedTime;
+    }
+    if (m_Input.isLeftPressed()) {
+        m_Position.x -= (m_Speed * elapsedTime) / 2.0f;
+    }
+    if (m_Input.isRightPressed()) {
+        m_Position.x += (m_Speed * elapsedTime) * 1.25f;
+    }
+    // Apply the new position to the sprite
+    m_Sprite.setPosition(m_Position);
 }
