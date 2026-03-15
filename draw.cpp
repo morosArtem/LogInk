ddfc65666f40cfce59e1315cb42f726ec44ae3ff
noble_kiwi <iwkr1208@gmail.com>
2026-03-15 16:26:03 +0300

    Update draw.cpp

--- draw.cpp
+++ draw.cpp
@@ -1,15 +1,15 @@
 #include "Engine.h"
 
-void Engine::draw()
-{
-   
+void Engine::draw() {
+    // Clear the window with a white background
     m_Window.clear(Color::White);
 
-    
+    // Draw background first
     m_Window.draw(m_BackgroundSprite);
-    
+
+    // Draw the car on top
     m_Window.draw(m_Car.getSprite());
 
-    
+    // Display everything
     m_Window.display();
 }
