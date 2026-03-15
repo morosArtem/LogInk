f6f6aea3c0d5bd66f2f07a6b58bc449a1a26b0b1
noble_kiwi <iwkr1208@gmail.com>
2026-03-15 16:03:43 +0300

    Update Car.h

--- Car.h
+++ Car.h
@@ -1,38 +1,23 @@
 #pragma once
 #include <SFML/Graphics.hpp>
-using namespace sf;
+#include "InputHandler.h"
 
-//класс для автoмобиля
+// Represents the player's car. It holds a sprite and updates its position
+// based on input from an InputHandler.
 class Car {
 private:
-	Vector2f m_Position;
+    sf::Sprite m_Sprite;          // The visual representation
+    sf::Vector2f m_Position;       // Current position in the world
+    float m_Speed;                 // Movement speed (pixels per second)
+    InputHandler& m_Input;          // Reference to the input provider
 
-	Sprite m_Sprite;
-	Texture m_Texture;
-
-	bool m_UpPressed;
-	bool m_DownPressed;
-	bool m_RightPressed;
-	bool m_LeftPressed;
-
-	float m_Speed;
 public:
-	Car();
+    // Constructor: takes an InputHandler reference that will be used to query keys.
+    Car(InputHandler& input);
 
-	Sprite getSprite();
+    // Updates the car's position based on elapsed time and current input.
+    void update(float elapsedTime);
 
-	void moveUp();
-	void moveDown();
-
-	void stopUp();
-	void stopDown();
-
-	void moveRight();
-	void moveLeft();
-
-	void stopRight();
-	void stopLeft();
-
-	void update(float elapsedTime);
+    // Returns the sprite for drawing.
+    sf::Sprite getSprite() const { return m_Sprite; }
 };
-
