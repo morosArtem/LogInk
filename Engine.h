7d46a44b42da96fa8b25a002fe3a8ffc3bcbc901
noble_kiwi <iwkr1208@gmail.com>
2026-03-15 16:30:32 +0300

    Update Engine.h

--- Engine.h
+++ Engine.h
@@ -1,27 +1,28 @@
 #pragma once
 #include <SFML/Graphics.hpp>
 #include "Car.h"
+#include "InputHandler.h"
 
 using namespace sf;
 
-class Engine
-{
+/**
+ * @class Engine
+ * @brief Main game engine class. Manages the window, game loop, and updates.
+ */
+class Engine {
 private:
-
     RenderWindow m_Window;
-
     Sprite m_BackgroundSprite;
     Texture m_BackgroundTexture;
 
-    Car m_Car;
+    InputHandler m_InputHandler;   // Handles key states
+    Car m_Car;                      // Player's car
 
     void input();
     void update(float dtAsSeconds);
     void draw();
 
 public:
-
     Engine();
     void start();
-
 };
